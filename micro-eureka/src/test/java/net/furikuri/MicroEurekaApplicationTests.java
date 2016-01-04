c4cb5f76e4cd1f17577471c101b25bc4adbc8790
Theo Pack Test <tf.pack@gmail.com>
2016-01-04 18:38:34 +0100

    Add eureka server

---
+++ micro-eureka/src/test/java/net/furikuri/MicroEurekaApplicationTests.java
@@ -0,0 +1,20 @@
+package net.furikuri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MicroEurekaApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port=0")
+public class MicroEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
