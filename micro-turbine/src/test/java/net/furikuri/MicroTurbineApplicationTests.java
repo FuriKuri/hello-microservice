5d9b6bdb366a84a0c0b8d863cb0012426b23068e
Theo Pack Test <tf.pack@gmail.com>
2016-01-04 18:47:57 +0100

    Add turbine

---
+++ micro-turbine/src/test/java/net/furikuri/MicroTurbineApplicationTests.java
@@ -0,0 +1,16 @@
+package net.furikuri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MicroTurbineApplication.class)
+public class MicroTurbineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
