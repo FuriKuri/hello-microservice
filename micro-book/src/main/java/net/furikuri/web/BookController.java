9c2da35f7e4e07ff65c62e2f75b0add8a8919f86
Theo Pack Test <tf.pack@gmail.com>
2016-01-06 21:09:16 +0100

    Use logstash

--- micro-book/src/main/java/net/furikuri/web/BookController.java
+++ micro-book/src/main/java/net/furikuri/web/BookController.java
@@ -1,16 +1,21 @@
 package net.furikuri.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
+    private Logger logger = LoggerFactory.getLogger(BookController.class);
+
     @Value("${eureka.instance.metadataMap.instanceId:-}")
     private String name;
 
     @RequestMapping("/")
     public String simple() {
+        logger.info("Got request");
         return "{ \"name\" : \"" + name + "\"}";
     }
 }
